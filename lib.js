9cc915204b914623834480488806a8a1b85e1c79
2h1082 <hongil0824@naver.com>
2023-11-01 23:23:06 +0900

    first commit

---
+++ lib.js
@@ -0,0 +1,22 @@
+function avg(numbers){
+    let s=numbers.reduce((prev,curr)=>prev+curr,0);
+    return s/numbers.length;
+}
+function prime(num){
+    
+    return true;
+}
+
+function fact(num){
+    return true;
+}
+module.exports={
+    avg,
+    prime,
+    fact
+}
+/*
+exports.avg=avg;
+exports.prime=prime;
+exports.fact=fact;
+*/
