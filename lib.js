8a6330e6514cb7913687f986a1f5738185ba47e4
2h1082 <hongil0824@naver.com>
2023-11-02 00:49:30 +0900

    last version implementing "prime", "fact"

--- lib.js
+++ lib.js
@@ -3,12 +3,36 @@ function avg(numbers){
     return s/numbers.length;
 }
 function prime(num){
-    //implement!
+    if (num <= 1) {
+        return false;
+    }
+    if (num <= 3) {
+        return true;
+    }
+    if (num % 2 === 0 || num % 3 === 0) {
+        return false;
+    }
+    
+    for (let i = 5; i * i <= num; i += 6) {
+        if (num % i === 0 || num % (i + 2) === 0) {
+            return false;
+        }
+    }
     return true;
 }
 
 function fact(num){
-    return true;
+    if (num === 0 || num === 1) {
+        return 1;
+    } else if (num < 0) {
+        return false;
+    } else {
+        let result = 1;
+        for (let i = 2; i <= num; i++) {
+            result *= i;
+        }
+        return result;
+    }
 }
 module.exports={
     avg,
